9187a19e7eb7a2f8ac45d2373a43117bb7038879
hykare <78385964+hykare@users.noreply.github.com>
2021-11-05 20:41:04 +0100

    check if string includes first word in the dictionary

---
+++ substrings.rb
@@ -0,0 +1,9 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dictionary)
+p string.include? dictionary[0]
+
+
+end
+
+substrings("below", dictionary)
