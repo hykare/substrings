ee0c7dfa0ebaa4d9ae97cbf56ff5e743ccee80f8
hykare <78385964+hykare@users.noreply.github.com>
2021-11-05 21:27:30 +0100

    count ocurrences of words in dictionary

--- substrings.rb
+++ substrings.rb
@@ -3,9 +3,9 @@ dictionary = %w[below down go going horn how howdy it i low own part partner sit
 def substrings(string, dictionary)
   output = Hash.new(0)
   dictionary.each do |word|
-    output[word] += 1 if string.include? word
+    output[word] = string.gsub(word).count if string.include? word
   end
   output
 end
 
-p substrings('down down', dictionary)
+p substrings("Howdy partner, sit down! How's it going", dictionary)
