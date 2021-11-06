3cbbaa4a2a40ad1f433beb768b1d27da0b51fe28
hykare <78385964+hykare@users.noreply.github.com>
2021-11-06 12:47:21 +0100

    case insensitive match

--- substrings.rb
+++ substrings.rb
@@ -3,7 +3,7 @@ dictionary = %w[below down go going horn how howdy it i low own part partner sit
 def substrings(string, dictionary)
   output = Hash.new(0)
   dictionary.each do |word|
-    output[word] = string.gsub(word).count if string.include? word
+    output[word] = string.gsub(/#{word}/i).count if string.match(/#{word}/i)
   end
   output
 end
